f6f73f41f1ce2f5eb7127bfecb7a5157418543ab
刘浩天 <2410651402@qq.com>
2024-08-20 00:56:30 +0800

    对象管理 持久化 保存 对象变体

---
+++ Assets/Scripts/ObjectManager/Shape.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class Shape : PersistableObject {
+
+    public int ShapeId
+    {
+        get
+        {
+            return shapeId;
+        }
+        set
+        {
+            if (shapeId == int.MinValue && value != int.MinValue)
+            {
+                shapeId = value;
+            }
+            else
+            {
+                Debug.LogError("Not allowed to change shapeId.");
+            }
+        }
+    }
+
+    int shapeId = int.MinValue;
+
+    public int MaterialId { get; private set; }
+
+    public void SetMaterial(Material material, int materialId)
+    {
+        meshRenderer.material = material;
+        MaterialId = materialId;
+    }
+
+    Color color;
+    static int colorPropertyId = Shader.PropertyToID("_BaseColor");
+    static MaterialPropertyBlock sharedPropertyBlock;
+    public void SetColor(Color color)
+    {
+        this.color = color;
+        if (sharedPropertyBlock == null)
+        {
+            sharedPropertyBlock = new MaterialPropertyBlock();
+        }
+        sharedPropertyBlock.SetColor(colorPropertyId, color);
+        meshRenderer.SetPropertyBlock(sharedPropertyBlock);
+    }
+
+
+    public override void Save(GameDataWriter writer)
+    {
+        base.Save(writer);
+        writer.Write(color);
+    }
+
+    public override void Load(GameDataReader reader)
+    {
+        base.Load(reader);
+        SetColor(reader.Version > 0 ? reader.ReadColor() : Color.white);
+    }
+
+    MeshRenderer meshRenderer;
+
+    void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+}
